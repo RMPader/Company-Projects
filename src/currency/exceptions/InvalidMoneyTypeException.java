a7357ecec81a46be79475fb747b0e901d530392f
clivelim <Clive.lim@orangeandbronze.com>
2012-06-22 13:19:44 +0800

    added 'add' function to money with test cases

--- src/currency/exceptions/InvalidMoneyTypeException.java
+++ src/currency/exceptions/InvalidMoneyTypeException.java
@@ -1,7 +1,7 @@
 package currency.exceptions;
 
-public class InvalidMoneyTypeException extends RuntimeException{
-	public InvalidMoneyTypeException(String message){
-			super(message);
-	}
+public class InvalidMoneyTypeException extends RuntimeException {
+    public InvalidMoneyTypeException(String message) {
+	super(message);
+    }
 }
